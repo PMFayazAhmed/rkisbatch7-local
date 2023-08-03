7aa9124b106086466bd453af978b80b9512f8a1d
Fayaz <fana.fayaz@gmail.com>
2023-08-03 07:41:18 +0530

    renamed variables

--- Addition.java
+++ Addition.java
@@ -2,10 +2,10 @@ class Addition
 {
 	public static void main(String args[])
 	{
-		int n1,n2,res;
-		n1=10;
-		n2=35;
-		res=n1+n2;
+		int num1,num2,res;
+		num1=10;
+		num2=35;
+		res=num11+num2;
 		System.out.println("Result is : "+res);
 	}
 }
